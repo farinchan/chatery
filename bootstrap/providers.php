fe6572f8e76e65345c203c823a236f01ed7094fa
Fajri Rinaldi Chan <fajri@gariskode.com>
2025-11-15 18:21:24 +0700

    Init Project

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    Jenssegers\Agent\AgentServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
+    RealRashid\SweetAlert\SweetAlertServiceProvider::class,
+];
